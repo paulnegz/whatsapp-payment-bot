0fabd58e0ae7897cd500f6bae0d4caaefc69e635
Paul Negedu <paul.negedu@yahoo.com>
2023-11-12 18:27:08 -0600

    start sever

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 from frontend import whatsapp
+from backend.server import app
+import uvicorn
 
 if __name__ == '__main__':
-    # start backend
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
     whatsapp.run()
